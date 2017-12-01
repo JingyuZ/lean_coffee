db5831bb2ce67b5a7ae193d58baeaf43dac4439c
Chris Horuk <chris.horuk@appfolio.com>
2017-12-01 00:18:16 -0800

    remove random controller

--- app/controllers/random_controller.rb
+++
@@ -1,7 +0,0 @@
-class RandomController < ApplicationController
-  before_action :authenticate_user!
-
-  def index
-
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'random#index'
+  root to: 'boards#index'
 
   resources :boards, only: [:index, :show]
   resources :subtopics, only: [] do
