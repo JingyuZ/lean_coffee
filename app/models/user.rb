2c8e421ccaec18b1233cbbcdc38425febb16c00f
Jingyu Zhang <jingyu.zhang@appfolio.com>
2017-11-30 16:43:47 -0800

    add first name and last name to users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates_presence_of :first_name, :last_name, :email, :encrypted_password
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
