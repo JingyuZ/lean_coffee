e303812f551a003227e7742e6f5f36e63d6f6d07
Jingyu Zhang <jingyu.zhang@appfolio.com>
2017-11-30 16:45:01 -0800

    log in with Google OAuth! woohoo we did it! need to use a better way to handle client_id and client_secret

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  devise :omniauthable, omniauth_providers: [:google_oauth2]
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+
+    unless user
+      user = User.create!(
+         first_name: data['first_name'],
+         last_name: data['last_name'],
+         email: data['email'],
+         password: Devise.friendly_token[0,20]
+      )
+    end
+    user
+  end
 end
