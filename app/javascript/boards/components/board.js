cc4f061af0ed438d76131a016b850c354703d862
Chris Horuk <chris.horuk@appfolio.com>
2018-01-12 10:28:43 -0800

    add some basic styling to boards index page

--- app/javascript/boards/components/board.js
+++ app/javascript/boards/components/board.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, CardBlock, CardText, CardTitle } from 'reactstrap';
+import '../styles/board';
 
 const Board = (props) => (
-  <Card className="mb-2" onClick={() => (props.onClick(props.id))}>
+  <Card className="mb-2 topic-board" onClick={() => (props.onClick(props.id))}>
     <CardBlock>
       <CardTitle>{props.topic}</CardTitle>
       <CardText>{props.description}</CardText>
